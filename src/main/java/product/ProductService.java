2c449ee7bd56b1061830bc92f54dceea71a2a511
Rafał Grzywacz <rafal.grzywacz@comarch.com>
2021-10-07 15:10:03 +0200

    first commit

---
+++ src/main/java/product/ProductService.java
@@ -0,0 +1,15 @@
+package product;
+
+public class ProductService {
+    public static double christmasDiscount(double cost){
+        return 0.15 * cost;
+    }
+
+    public static double easterDiscount(double cost){
+        return 0.10 * cost;
+    }
+
+    public static double newYearDiscount(double cost){
+        return 0.2 * cost;
+    }
+}
